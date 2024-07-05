4d20eb75fe4a2d74adc5e96681016c2ba11452ab
Richard <olugbenga.rich@gmail.com>
2024-07-05 07:05:54 +0100

    Created header page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import LoginPage from "./LoginPage";
+import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
   return (
---
+++ src/HeaderArea/HeaderArea.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HeaderArea.css";
+
+function HeaderArea() {
+  return <div>Welcome to facebook</div>;
+}
+
+export default HeaderArea;
